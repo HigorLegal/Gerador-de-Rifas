62131fd2c9c839029b954df07f05c87c6ae51c09
HigorLegal <146496157+HigorLegal@users.noreply.github.com>
2024-11-08 21:36:30 -0300

    Create README.d

---
+++ README.d
@@ -0,0 +1,59 @@
+# Projeto Gerador de Rifas
+
+projeto feito na materia de web 2
+
+![Banner do
+Projeto](print da tela do site)
+
+## Sobre o Projeto
+
+O **Gerador de Rifas** é um sistema que verifica um formulario preenchido pelo usuario e cria uma pagina com a quantidade de rifas que o usuario pediu e com informaçoes como o premio, o valor da rifa, a imagem do premio e a data de excuçao .Tambem contem espaços para colocar as informaçoes do participante da rifa e o numero da rifa do participante da rifa.Este projeto utiliza **HTML**, **CSS** e **PHP** para criar uma ferramenta para a criaçao de rifas.
+
+## Visão Geral
+
+### Estrutura do Projeto
+
+- **formulario** : um formulario onde o ususario ira colocar as informaçoes sobre a rifa. 
+- **pagina de impreçao** : uma pagina que criara as rifas com a quantidade que o usuario pediu.
+
+## Pré-requisitos
+
+- **Git**: Para clonar o repositório.
+- **Navegador**: Google Chrome, Firefox, Safari, etc., para visualizar
+  o site.
+- **Editor de Código**: Recomendamos o Visual Studio Code ou o Sublime
+  Text.
+
+## Tecnologias Utilizadas
+
+As tecnologias e ferramentas utilizadas neste projeto são:
+
+- **HTML5**: Estruturação semântica e organizada.
+- **CSS3**: Estilos visuais.
+- **PHP**: Validação de formulários.
+
+
+
+## Estrutura de Pastas
+
+```plaintext
+/Gerador-de-Rifas
+│
+├── geradorRifa.PHP # Página do formulario 
+├── rifa.PHP # Página de impreçao das rifas
+└── README.md # Documentação do projeto
+
+## Como Executar o Projeto
+1. Clone o repositório em sua máquina local:
+git clone https://github.com/seu_usuario/Gerador-de-Rifas.git
+2. Navegue até o diretório do projeto:
+cd Gerador-de-Rifas
+3. Abra o arquivo geradorRifas.PHP em seu navegador.
+
+Dica: Você pode usar uma extensão de servidor local no VS Code, como
+Live Server, para visualizar o projeto com recarregamento automático.
+
+## Desenvolvedor
+ Nome do Aluno: Higor da Silva Gomes de Oliveira 
+ Contato: higorgom002@gmail.com
+```
